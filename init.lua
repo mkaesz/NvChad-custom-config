b8884a39fcde869ee2f6fbd6ef3f86dd7de2f01a
Marc-Steffen Kaesz <mkaesz@msk.pub>
2023-12-06 20:10:12 +0100

    added custom key mappings

--- init.lua
+++ init.lua
@@ -1,4 +1,21 @@
-vim.wo.relativenumber = true
+-- :help options
+vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.opt.pumheight = 10                          -- pop up menu height
+vim.opt.showtabline = 2                         -- always show tabs
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.swapfile = false                        -- creates a swapfile
+vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
+vim.opt.relativenumber = true                   -- set relative numbered lines
+vim.opt.wrap = false                            -- display lines as one long line
+vim.opt.scrolloff = 10                          -- is one of my fav
+vim.opt.sidescrolloff = 8
+vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 vim.opt.colorcolumn = "80"
 
 
--- mappings.lua
+++ mappings.lua
@@ -18,4 +18,24 @@ M.dap_python = {
   }
 }
 
+M.msk = {
+  n = {
+    -- Resize with arrows
+    ["<C-Up>"] = {"<cmd> resize +2 <CR>", "Resize with arrow"},
+    ["<C-Down>"] = {"<cmd> resize -2 <CR>", "Resize with arrow"},
+    ["<C-Left>"] = {"<cmd> vertical resize +2 <CR>", "Resize with arrow"},
+    ["<C-Right>"] = {"<cmd> vertical resize +2 <CR>", "Resize with arrow"},
+    -- Navigate buffers
+    ["<S-l>"] = {"<cmd> bnext <CR>", "Jump to next buffer"},
+    ["<S-h>"] = {"<cmd> bprevious <CR>", "Jump to previous buffer"},
+  },
+  i = {},
+  v = {
+    ["<"] = { "<gv"},
+    [">"] = { ">gv"},
+  },
+  x = {},
+  t = {},
+}
+
 return M
